65ac6c155199f689f6693ecc21a16c51757eb14c
xBrownieCodez <72617433+xBrownieCodezV2@users.noreply.github.com>
2021-03-31 18:34:02 +0200

    Fix and clean MainGUI

--- src/me/levansj01/verus/gui/impl/MainGUI.java
+++ src/me/levansj01/verus/gui/impl/MainGUI.java
@@ -4,6 +4,7 @@ import com.google.common.collect.ImmutableSet;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 import java.util.Set;
 import java.util.UUID;
 import me.levansj01.verus.VerusPlugin;
@@ -23,169 +24,146 @@ import me.levansj01.verus.util.item.MaterialList;
 import me.levansj01.verus.util.java.WordUtils;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.command.CommandSender;
 import org.bukkit.entity.HumanEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.metadata.Metadatable;
 
 public class MainGUI extends GUI {
-   private static final String format;
-   private static final String verusType;
-   private static final String build;
-   public static final Set<UUID> ALLOWED_UUIDS = ImmutableSet.of(
-           /*
-           UUID.fromString("564b7f5e-9efc-4b18-9ff3-9ea62117b113"),
-           UUID.fromString("34272ea9-008d-4cb0-9990-4085d7048ddb"),
-           UUID.fromString("79295695-5fa7-4fa1-bc68-a16b09de873d"),
-           UUID.fromString("954b39b2-2161-45ae-8b7d-b61c81109621"),
-           UUID.fromString("fb467fbd-a6fc-4114-ac4e-86c93dd17f4c")
-           */
-   );
-   private static final ItemStack infoStack;
-   private static final String implementation;
-   private static final ItemStack blank;
+    private static final String format = VerusPlugin.COLOR + "Total %s %s";
+    private static final String verusType = WordUtils
+            .capitalize((String) VerusTypeLoader.getVerusType().name().toLowerCase());
+    private static final String build = String.valueOf(VerusPlugin.getBuild());
+    public static final Set<UUID> ALLOWED_UUIDS = ImmutableSet.of(UUID.fromString(""));
+    private static final ItemStack infoStack = new ItemStack(MaterialList.PAPER);
+    private static final String implementation = Bukkit.getServer().getClass().getName().split("\\.")[3];
+    private static final ItemStack blank = new ItemBuilder().setType(MaterialList.STAINED_GLASS_PANE).setName(" ")
+            .build();
 
-   public void onClick(InventoryClickEvent var1) {
-      int var2 = var1.getSlot();
-      HumanEntity var3 = var1.getWhoClicked();
-      if (var3 instanceof Player) {
-         Player var4 = (Player)var3;
-         if (var2 == 0) {
-            if (ALLOWED_UUIDS.contains(var4.getUniqueId()) || var4.getName().equals("Quantise") || var4.getName().equals("Cupo")) {
-               PlayerData var5 = DataManager.getInstance().getPlayer(var4);
-               boolean var10001;
-               if (!var5.isDebug()) {
-                  var10001 = true;
-               } else {
-                  var10001 = false;
-               }
+    public void onClick(InventoryClickEvent inventoryClickEvent) {
+        int n = inventoryClickEvent.getSlot();
+        HumanEntity humanEntity = inventoryClickEvent.getWhoClicked();
+        if (humanEntity instanceof Player) {
+            Player player = (Player) humanEntity;
+            if (n == 0) {
+                if (ALLOWED_UUIDS.contains((Object) player.getUniqueId())
+                        || player.getName().equals((Object) "Quantise") || player.getName().equals((Object) "Cupo")) {
+                    String string;
+                    boolean bl;
+                    PlayerData playerData = DataManager.getInstance().getPlayer(player);
+                    if (!playerData.isDebug()) {
+                        bl = true;
+                    } else {
+                        bl = false;
+                    }
+                    playerData.setDebug(bl);
+                    BukkitUtil.setMeta((Metadatable) player, (String) "verus.admin", (boolean) playerData.isDebug());
+                    StringBuilder stringBuilder = new StringBuilder().append((Object) VerusPlugin.COLOR)
+                            .append("You are ");
+                    if (playerData.isDebug()) {
+                        string = "now";
 
-               var5.setDebug(var10001);
-               BukkitUtil.setMeta(var4, "verus.admin", var5.isDebug());
-               StringBuilder var6 = (new StringBuilder()).append(VerusPlugin.COLOR).append("You are ");
-               String var10002;
-               if (var5.isDebug()) {
-                  var10002 = "now";
-               } else {
-                  var10002 = "no longer";
-               }
-               var4.sendMessage(var6.append(var10002).append(" in debug mode").toString());
+                    } else {
+                        string = "no longer";
+                    }
+                    player.sendMessage(stringBuilder.append(string).append(" in debug mode").toString());
+
+                }
+            } else if (n == 11) {
+                GUIManager.getInstance().getCheckGui().openGui(player);
+            } else if (n == 13) {
+                this.updateInfoStack();
+            } else if (n == 15 && BukkitUtil.hasPermission((CommandSender) humanEntity, (String) "verus.restart")) {
+                PacketManager.getInstance().postToMainThread(VerusPlugin::restart);
             }
-         } else if (var2 == 11) {
-            GUIManager.getInstance().getCheckGui().openGui(var4);
-         } else if (var2 == 13) {
-            this.updateInfoStack();
-         } else if (var2 == 15 && BukkitUtil.hasPermission(var3, "verus.restart")) {
-            PacketManager.getInstance().postToMainThread(VerusPlugin::restart);
-         }
-      }
+        }
+    }
 
-   }
+    public MainGUI() {
+        super(VerusPlugin.COLOR + VerusPlugin.getNameFormatted() + " AntiCheat", Integer.valueOf((int) 27));
+        for (int i = 0; i < this.inventory.getSize(); ++i) {
+            if (i > 9 && i < 17) {
 
-   public MainGUI() {
-      super(VerusPlugin.COLOR + VerusPlugin.getNameFormatted() + " AntiCheat", 27);
-      int var1 = 0;
+            }
+            this.inventory.setItem(i, blank);
+        }
+        ArrayList<String> arrayList = new ArrayList<String>();
+        arrayList.add(" ");
+        for (CheckType checkType : CheckType.values()) {
+            if (checkType == CheckType.MANUAL) {
+            }
+            arrayList.add(
+                    ChatColor.WHITE + checkType.getName() + ChatColor.GRAY + " (ID: " + checkType.getSuffix() + ")");
+        }
+        arrayList.add(" ");
+        arrayList.add(ChatColor.GRAY + "Click to open Checks menu");
+        this.inventory.setItem(11, new ItemBuilder(MaterialList.BOOK).setName(VerusPlugin.COLOR + "Checks")
+                .setLore(arrayList).setAmount(CheckType.values().length - 1).build());
+        this.updateInfoStack();
+        this.inventory.setItem(15,
+                (new ItemBuilder(MaterialList.REDSTONE)).setName(VerusPlugin.COLOR + "Restart")
+                        .setLore(Collections
+                                .singletonList(ChatColor.GRAY + "Click to Restart " + VerusPlugin.getNameFormatted()))
+                        .build());
+    }
 
-      do {
-         if (var1 >= this.inventory.getSize()) {
-            ArrayList<String> var6 = new ArrayList<>();
-            var6.add(" ");
-            CheckType[] var2 = CheckType.values();
-            int var3 = var2.length;
-            int var4 = 0;
-
-            do {
-               if (var4 >= var3) {
-                  var6.add(" ");
-                  var6.add(ChatColor.GRAY + "Click to open Checks menu");
-                  this.inventory.setItem(11, (new ItemBuilder(MaterialList.BOOK)).setName(VerusPlugin.COLOR + "Checks").setLore(var6).setAmount(CheckType.values().length - 1).build());
-                  this.updateInfoStack();
-                  this.inventory.setItem(15, (new ItemBuilder(MaterialList.REDSTONE)).setName(VerusPlugin.COLOR + "Restart").setLore(Collections.singletonList(ChatColor.GRAY + "Click to Restart " + VerusPlugin.getNameFormatted())).build());
-                  return;
-               }
-
-               CheckType var5 = var2[var4];
-               if (var5 == CheckType.MANUAL) {
-               } else {
-                  var6.add(ChatColor.WHITE + var5.getName() + ChatColor.GRAY + " (ID: " + var5.getSuffix() + ")");
-               }
-
-               ++var4;
-            } while(true);
-
-         }
-
-         if (var1 > 9 && var1 < 17) {
-         } else {
-            this.inventory.setItem(var1, blank);
-         }
-
-         ++var1;
-      } while(true);
-
-   }
-
-   static {
-      infoStack = new ItemStack(MaterialList.PAPER);
-      blank = (new ItemBuilder()).setType(MaterialList.STAINED_GLASS_PANE).setName(" ").build();
-      verusType = WordUtils.capitalize(VerusTypeLoader.getVerusType().name().toLowerCase());
-      build = String.valueOf(VerusPlugin.getBuild());
-      implementation = Bukkit.getServer().getClass().getName().split("\\.")[3];
-      format = VerusPlugin.COLOR + "Total %s %s";
-   }
-
-   public void updateInfoStack() {
-      ItemMeta var1 = infoStack.getItemMeta();
-      var1.setDisplayName(VerusPlugin.COLOR + VerusPlugin.getNameFormatted() + " Information");
-      StorageEngine var2 = StorageEngine.getInstance();
-      DatabaseType var3 = var2.getType();
-      API var4 = API.getAPI();
-      String[] var10001 = new String[]{"", VerusPlugin.COLOR + "Type " + ChatColor.WHITE + " Premium" + ChatColor.AQUA + " (Cracked by xBrownieCodez)", null, null, null, null, null, null, null, null};
-      StringBuilder var10004 = (new StringBuilder()).append(VerusPlugin.COLOR).append("Build ").append(ChatColor.WHITE).append(build);
-      String var10005;
-      if (var4 == null) {
-         var10005 = "";
-      } else {
-         var10005 = ChatColor.GRAY + " (API v" + var4.getVersion() + ")";
-      }
-
-      var10001[2] = var10004.append(var10005).toString();
-      var10001[3] = VerusPlugin.COLOR + "Implementation " + ChatColor.WHITE + implementation;
-      var10004 = (new StringBuilder()).append(VerusPlugin.COLOR).append("Storage: ").append(ChatColor.WHITE);
-      if (var3 != null) {
-         var10005 = var3.name();
-      } else {
-         var10005 = "None";
-      }
-
-      var10001[4] = var10004.append(var10005).toString();
-      var10001[5] = "";
-      String var6 = format;
-      Object[] var5 = new Object[]{"Bans", null};
-      String var10008;
-      if (var2.isConnected()) {
-         var10008 = ChatColor.WHITE + String.valueOf(var2.getDatabase().getTotalBans());
-      } else {
-         var10008 = ChatColor.RED + "Not Connected";
-      }
-
-      var5[1] = var10008;
-      var10001[6] = String.format(var6, var5);
-      var6 = format;
-      var5 = new Object[]{"Logs", null};
-      if (var2.isConnected()) {
-         var10008 = ChatColor.WHITE + String.valueOf(var2.getDatabase().getTotalLogs());
-      } else {
-         var10008 = ChatColor.RED + "Not Connected";
-      }
-
-      var5[1] = var10008;
-      var10001[7] = String.format(var6, var5);
-      var10001[8] = "";
-      var10001[9] = ChatColor.GRAY + "Click to Refresh";
-      var1.setLore(Arrays.asList(var10001));
-      infoStack.setItemMeta(var1);
-      this.inventory.setItem(13, infoStack);
-   }
+    public void updateInfoStack() {
+        String string;
+        String string2;
+        String string3;
+        String string4;
+        ItemMeta itemMeta = infoStack.getItemMeta();
+        itemMeta.setDisplayName(VerusPlugin.COLOR + VerusPlugin.getNameFormatted() + " Information");
+        StorageEngine storageEngine = StorageEngine.getInstance();
+        DatabaseType databaseType = storageEngine.getType();
+        API aPI = API.getAPI();
+        String[] objectArray = new String[10];
+        objectArray[0] = "";
+        objectArray[1] = VerusPlugin.COLOR + "Type " + ChatColor.WHITE + " Premium" + ChatColor.AQUA
+                + " (Cracked by xBrownieCodez)";
+        StringBuilder stringBuilder = new StringBuilder().append((Object) VerusPlugin.COLOR).append("Build ")
+                .append((Object) ChatColor.WHITE).append(build);
+        if (aPI == null) {
+            string4 = "";
+        } else {
+            string4 = ChatColor.GRAY + " (API v" + aPI.getVersion() + ")";
+        }
+        objectArray[2] = stringBuilder.append(string4).toString();
+        objectArray[3] = VerusPlugin.COLOR + "Implementation " + ChatColor.WHITE + implementation;
+        StringBuilder stringBuilder2 = new StringBuilder().append((Object) VerusPlugin.COLOR).append("Storage: ")
+                .append((Object) ChatColor.WHITE);
+        if (databaseType != null) {
+            string3 = databaseType.name();
+        } else {
+            string3 = "None";
+        }
+        objectArray[4] = stringBuilder2.append(string3).toString();
+        objectArray[5] = "";
+        Object[] objectArray2 = new Object[2];
+        objectArray2[0] = "Bans";
+        if (storageEngine.isConnected()) {
+            string2 = ChatColor.WHITE + String.valueOf((int) storageEngine.getDatabase().getTotalBans());
+        } else {
+            string2 = ChatColor.RED + "Not Connected";
+        }
+        objectArray2[1] = string2;
+        objectArray[6] = String.format((String) format, (Object[]) objectArray2);
+        Object[] objectArray3 = new Object[2];
+        objectArray3[0] = "Logs";
+        if (storageEngine.isConnected()) {
+            string = ChatColor.WHITE + String.valueOf((int) storageEngine.getDatabase().getTotalLogs());
+        } else {
+            string = ChatColor.RED + "Not Connected";
+        }
+        objectArray3[1] = string;
+        objectArray[7] = String.format((String) format, (Object[]) objectArray3);
+        objectArray[8] = "";
+        objectArray[9] = ChatColor.GRAY + "Click to Refresh";
+        itemMeta.setLore(Arrays.asList(objectArray));
+        infoStack.setItemMeta(itemMeta);
+        this.inventory.setItem(13, infoStack);
+    }
 }
