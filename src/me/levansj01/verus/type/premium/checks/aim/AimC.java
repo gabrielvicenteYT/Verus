395a204f3faa0421461d960a2e0146b51cb5c1a2
xBrownieCodez <72617433+xBrownieCodezV2@users.noreply.github.com>
2021-03-31 05:22:46 +0200

    Push AimC (Premium Check)

---
+++ src/me/levansj01/verus/type/premium/checks/aim/AimC.java
@@ -0,0 +1,22 @@
+package me.levansj01.verus.type.premium.checks.aim;
+
+import me.levansj01.verus.check.AimCheck;
+import me.levansj01.verus.check.annotation.CheckInfo;
+import me.levansj01.verus.check.type.CheckType;
+import me.levansj01.verus.check.version.CheckVersion;
+import me.levansj01.verus.util.PlayerLocation;
+
+@CheckInfo(type = CheckType.AIM_ASSIST, subType = "C", friendlyName = "Aim", version = CheckVersion.RELEASE, minViolations = -2.5, logData = true)
+public class AimC extends AimCheck {
+    public void handle(PlayerLocation playerLocation, PlayerLocation playerLocation2, long l) {
+        if (this.playerData.getLastAttackTicks() > 100) {
+            return;
+        }
+        float f = Math.abs(playerLocation2.getYaw() - playerLocation.getYaw());
+        float f2 = Math.abs(playerLocation2.getPitch() - playerLocation.getPitch());
+        if (f > 0.0f && (double) f < 0.01 && (double) f2 > 0.2) {
+            this.handleViolation(String.format("Y: %s P: %s", new Object[] { Float.valueOf(f), Float.valueOf(f2) }));
+        }
+        this.violations -= Math.min(this.violations + 2.5, 0.05);
+    }
+}
