d28f0449983ba8a405aeccf1e34532748da5d098
xBrownieCodez <72617433+xBrownieCodezV2@users.noreply.github.com>
2021-03-31 05:28:24 +0200

    Push AimJ (Premium Check)

---
+++ src/me/levansj01/verus/type/premium/checks/aim/AimJ.java
@@ -0,0 +1,25 @@
+package me.levansj01.verus.type.premium.checks.aim;
+
+import me.levansj01.verus.check.AimCheck;
+import me.levansj01.verus.check.type.CheckType;
+import me.levansj01.verus.check.version.CheckVersion;
+import me.levansj01.verus.util.PlayerLocation;
+
+public class AimJ extends AimCheck {
+    public void handle(PlayerLocation playerLocation, PlayerLocation playerLocation2, long l) {
+        float f = Math.abs(playerLocation.getYaw() - playerLocation2.getYaw());
+        float f2 = Math.abs(playerLocation.getPitch() - playerLocation2.getPitch());
+        if (this.playerData.getLastAttackTicks() < 200) {
+            if (f2 > 0.0f && f > 2.0f && (double) f2 < 0.0119) {
+                this.handleViolation(
+                        String.format("Y: %s P: %s", new Object[] { Float.valueOf(f), Float.valueOf(f2) }));
+            }
+            this.violations -= Math.min(this.violations + 2.0, 0.001);
+        }
+    }
+
+    public AimJ() {
+        super(CheckType.AIM_ASSIST, "J", "Aim", CheckVersion.EXPERIMENTAL);
+        this.violations = -2.0;
+    }
+}
