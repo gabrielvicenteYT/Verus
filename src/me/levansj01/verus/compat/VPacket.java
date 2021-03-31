e1633681b1aa8f081bb95bde165f3508647537fe
xBrownieCodez <72617433+xBrownieCodezV2@users.noreply.github.com>
2021-03-31 09:57:19 +0200

    Push VPacket (WIP)

---
+++ src/me/levansj01/verus/compat/VPacket.java
@@ -0,0 +1,14 @@
+
+package me.levansj01.verus.compat;
+
+import java.util.function.Consumer;
+
+public abstract class VPacket implements Consumer<Object> {
+    // TODO Figure this "P" out
+ /*   public P create() {
+        throw new UnsupportedOperationException();
+    }*/
+
+    public void done() {
+    }
+}
