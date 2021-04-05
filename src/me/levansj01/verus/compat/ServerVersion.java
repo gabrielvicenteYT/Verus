42a96e47748a19d624c191b0afbce5c2c4bf1157
xBrownieCodez <72617433+xBrownieCodezV2@users.noreply.github.com>
2021-04-05 09:42:12 +0200

    Update ServerVersion.java

--- src/me/levansj01/verus/compat/ServerVersion.java
+++ src/me/levansj01/verus/compat/ServerVersion.java
@@ -1,17 +1,26 @@
 package me.levansj01.verus.compat;
 
-public enum ServerVersion
-{
-    v1_11_R1, 
-    v1_7_R4, 
-    v1_8_R3, 
-    v1_12_R1;
-    
-    public boolean after(final ServerVersion serverVersion) {
-        return this.ordinal() > serverVersion.ordinal();
-    }
-    
-    public boolean before(final ServerVersion serverVersion) {
+public enum ServerVersion {
+    v1_7_R4,
+    v1_8_R3,
+    v1_11_R1,
+    v1_12_R1,
+    v1_14_R1,
+    v1_15_R1,
+    v1_16_R1,
+    v1_16_R2,
+    v1_16_R3;
+
+
+    public boolean before(ServerVersion serverVersion) {
         return serverVersion.ordinal() > this.ordinal();
+    }
+
+    public boolean afterOrEq(ServerVersion serverVersion) {
+        return this.ordinal() >= serverVersion.ordinal();
+    }
+
+    public boolean after(ServerVersion serverVersion) {
+        return this.ordinal() > serverVersion.ordinal();
     }
 }
