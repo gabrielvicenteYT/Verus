190bc2d47f91a84675f2f6b7f3e54de9b0582f6f
xBrownieCodez <72617433+xBrownieCodezV2@users.noreply.github.com>
2021-04-05 09:49:43 +0200

    Fix NMSManager class

--- src/me/levansj01/verus/compat/NMSManager.java
+++ src/me/levansj01/verus/compat/NMSManager.java
@@ -2,83 +2,108 @@ package me.levansj01.verus.compat;
 
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
-
+import me.levansj01.verus.compat.BlockPosition;
+import me.levansj01.verus.compat.ServerVersion;
+import me.levansj01.verus.data.PlayerData;
+import me.levansj01.verus.data.version.ClientVersion;
+import me.levansj01.verus.util.Cuboid;
+import me.levansj01.verus.util.MutableBlockLocation;
 import org.bukkit.Bukkit;
+import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.World;
 import org.bukkit.block.Block;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.material.MaterialData;
 
-import me.levansj01.verus.data.PlayerData;
-import me.levansj01.verus.data.version.ClientVersion;
-import me.levansj01.verus.util.MutableBlockLocation;
-
-public abstract class NMSManager
-{
-    private static NMSManager nmsManager;
+public abstract class NMSManager {
+    private static NMSManager nmsManager = null;
     private ServerVersion serverVersion;
-    
+
+    public abstract boolean setInWater(Player var1, boolean var2);
+
+    public void setServerVersion(ServerVersion serverVersion) {
+        this.serverVersion = serverVersion;
+    }
+
+    public abstract double getMovementSpeed(Player var1);
+
+    public abstract ClientVersion getVersion(Player var1);
+
+    public abstract void setPing(Player var1, int var2);
+
+    public abstract MaterialData getTypeAndData(Player var1, World var2, MutableBlockLocation var3);
+
+    public abstract Material getType(Player var1, World var2, MutableBlockLocation var3);
+
+    public abstract int getCurrentTick();
+
+    public abstract void sendTransaction(Player var1, short var2);
+
     private static NMSManager newInstance() {
         try {
-            final String s = Bukkit.getServer().getClass().getName().split("\\.")[3];
-            final NMSManager nmsManager = (NMSManager)Class.forName(NMSManager.class.getName().replace(".NMSManager", "." + s + ".NMSManager")).newInstance();
-            nmsManager.setServerVersion(ServerVersion.valueOf(s));
-            return nmsManager;
+            String string = Bukkit.getServer().getClass().getName().split("\\.")[3];
+            NMSManager nMSManager = (NMSManager)Class.forName((String)NMSManager.class.getName().replace((CharSequence)".NMSManager", (CharSequence)("." + string + ".NMSManager"))).newInstance();
+            nMSManager.setServerVersion(ServerVersion.valueOf((String)string));
+            return nMSManager;
         }
-        catch (Throwable t) {
-            throw new IllegalArgumentException(t);
+        catch (Throwable throwable) {
+            throw new IllegalArgumentException(throwable);
         }
     }
-    
-    public static NMSManager getInstance() {
-        return (NMSManager.nmsManager == null) ? (NMSManager.nmsManager = newInstance()) : NMSManager.nmsManager;
-    }
-    
-    public abstract Material getType(final Player p0, final World p1, final MutableBlockLocation p2);
-    
-    public abstract double getMovementSpeed(final Player p0);
-    
-    public <K, V> Map<K, V> createCache(final Long n, final Long n2) {
+
+    public abstract float getFrictionFactor(Player var1, World var2, MutableBlockLocation var3);
+
+    public <K, V> Map<K, V> createCache(Long l, Long l2) {
         return new ConcurrentHashMap<K, V>();
     }
-    
+
+    public abstract float getFrictionFactor(Block var1);
+
+    public boolean isEmpty(World world, Entity entity, Cuboid cuboid, MutableBlockLocation mutableBlockLocation) {
+        throw new UnsupportedOperationException();
+    }
+
+    public boolean rayTrace(World world, double d, double d2, double d3, double d4, double d5, double d6, boolean bl, boolean bl2, boolean bl3) {
+        throw new UnsupportedOperationException();
+    }
+
+    public abstract boolean setOnGround(Player var1, boolean var2);
+
+    public abstract void sendBlockUpdate(Player var1, Location var2);
+
+    public boolean isGliding(Player player) {
+        return false;
+    }
+
+    public abstract int sendFakeEntity(PlayerData var1, PlayerData var2);
+
     public ServerVersion getServerVersion() {
         return this.serverVersion;
     }
-    
-    public void setServerVersion(final ServerVersion serverVersion) {
-        this.serverVersion = serverVersion;
+
+    public void setAsyncPotionEffects(Player player) {
     }
-    
-    public abstract MaterialData getTypeAndData(final Player p0, final World p1, final MutableBlockLocation p2);
-    
-    public abstract void sendPayload(final Player p0, final String p1);
-    
-    public abstract void sendDestroyPacket(final Player p0, final int p1);
-    
-    public abstract float getDamage(final Player p0, final World p1, final BlockPosition p2);
-    
-    public abstract void setPing(final Player p0, final int p1);
-    
-    public abstract float getFrictionFactor(final Block p0);
-    
-    public abstract ClientVersion getVersion(final Player p0);
-    
-    public abstract float getFrictionFactor(final Player p0, final World p1, final MutableBlockLocation p2);
-    
-    public abstract int sendFakeEntity(final PlayerData p0, final PlayerData p1);
-    
-    public abstract boolean isOnElytra(final Player p0);
-    
-    public void setAsyncPotionEffects(final Player player) {
+
+    public abstract double getItemModifier(ItemStack var1);
+
+    public boolean isRiptiding(Player player) {
+        return false;
     }
-    
-    public abstract int getCurrentTick();
-    
-    public abstract void sendTransaction(final Player p0, final short p1);
-    
-    static {
-        NMSManager.nmsManager = null;
+
+    public abstract Cuboid getBoundingBox(Player var1, World var2, MutableBlockLocation var3);
+
+    public abstract boolean isLoaded(World var1, int var2, int var3);
+
+    public abstract float getDamage(Player var1, World var2, BlockPosition var3);
+
+    public static NMSManager getInstance() {
+        return nmsManager == null ? (nmsManager = NMSManager.newInstance()) : nmsManager;
+    }
+
+    public ItemStack getOffHand(Player player) {
+        return null;
     }
 }
